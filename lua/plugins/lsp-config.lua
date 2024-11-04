d5e10fea1a3b1d5f8bdd7e8fd1c23d70505549fd
eirikkar <eirikkar@proton.me>
2024-11-04 21:21:19 +0100

    added several more configs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,103 +1,61 @@
 return {
-  -- Mason and Mason LSPConfig
-  {
-    'williamboman/mason.nvim',
-    config = function()
-      require('mason').setup()
-    end
-  },
-  {
-    'williamboman/mason-lspconfig.nvim',
-    dependencies = { 'williamboman/mason.nvim', 'neovim/nvim-lspconfig' },
-    config = function()
-      require('mason-lspconfig').setup({
-        ensure_installed = { 'omnisharp' },
-        automatic_installation = true,
-      })
-    end
-  },
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		opts = {
+			auto_install = true,
+			ensure_installed = { "omnisharp", "lua_ls" },
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+			local lspconfig = require("lspconfig")
+			local mason_registry = require("mason-registry")
 
-  -- LSP Config
-  {
-    'neovim/nvim-lspconfig',
-  },
+			-- Retrieve the Omnisharp executable path
+			local omnisharp_pkg = mason_registry.get_package("omnisharp")
+			local omnisharp_path = omnisharp_pkg:get_install_path() .. "/omnisharp"
 
-  -- nvim-dap
-  {
-    'mfussenegger/nvim-dap',
-  },
+			-- Configure Omnisharp
+			lspconfig.omnisharp.setup({
+				cmd = { omnisharp_path, "--languageserver", "--hostPID", tostring(vim.fn.getpid()) },
+				capabilities = capabilities,
+			})
 
-  -- structlog.nvim
- { 
-    "Tastyep/structlog.nvim" 
- },
+			-- Configure lua_ls
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						diagnostics = {
+							globals = { "vim" },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
 
-  -- csharp.nvim
-{
-  'iAbdelkareem/csharp.nvim',
-  dependencies = {
-    'neovim/nvim-lspconfig',
-    'williamboman/mason.nvim',
-    'mfussenegger/nvim-dap',
-    'Tastyep/structlog.nvim', 
-  },
-    config = function()
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-
-      require('csharp').setup({
-        server = {
-          capabilities = capabilities,
-          on_attach = function(client, bufnr)
-            local opts = { buffer = bufnr, silent = true }
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-            -- Add more keybindings as needed
-          end,
-        }
-      })
-    end
-  },
-
-  -- Autocompletion Plugins
-  {
-    'hrsh7th/nvim-cmp',
-    event = 'InsertEnter',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'saadparwaiz1/cmp_luasnip',
-      'L3MON4D3/LuaSnip',
-    },
-    config = function()
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'luasnip' },
-        }, {
-          { name = 'buffer' },
-          { name = 'path' },
-        })
-      })
-
-      -- Load LuaSnip snippets
-      require('luasnip.loaders.from_vscode').lazy_load()
-    end,
-  },
+			-- Key mappings
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
