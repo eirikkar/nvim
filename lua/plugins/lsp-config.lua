ecbd3c47b2e4aeec36aeb3525de70aa77d0c24cb
eirikkar <eirikkar@proton.me>
2024-11-04 19:54:48 +0100

    added several configs

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,94 +1,103 @@
 return {
+  -- Mason and Mason LSPConfig
   {
-    "williamboman/mason.nvim",
-    lazy = false,
+    'williamboman/mason.nvim',
     config = function()
-      require("mason").setup()
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    lazy = false,
-    opts = {
-      auto_install = true,
-
-    },
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "omnisharp" }
-      })
+      require('mason').setup()
     end
   },
   {
-    "neovim/nvim-lspconfig",
-    lazy = false,
+    'williamboman/mason-lspconfig.nvim',
+    dependencies = { 'williamboman/mason.nvim', 'neovim/nvim-lspconfig' },
     config = function()
-
-      local lspconfig = require("lspconfig")
-
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities
+      require('mason-lspconfig').setup({
+        ensure_installed = { 'omnisharp' },
+        automatic_installation = true,
       })
-			lspconfig.omnisharp.setup({
-				capabilities = capabilities,
-				enable_roslyn_analysers = true,
-				enable_import_completion = true,
-				organize_imports_on_format = true,
-				enable_decompilation_support = true,
-				filetypes = { 'cs', 'vb', 'csproj', 'sln', 'slnx', 'props', 'csx', 'targets' }
-			})
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+  },
+
+  -- LSP Config
+  {
+    'neovim/nvim-lspconfig',
+  },
+
+  -- nvim-dap
+  {
+    'mfussenegger/nvim-dap',
+  },
+
+  -- structlog.nvim
+ { 
+    "Tastyep/structlog.nvim" 
+ },
+
+  -- csharp.nvim
+{
+  'iAbdelkareem/csharp.nvim',
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'williamboman/mason.nvim',
+    'mfussenegger/nvim-dap',
+    'Tastyep/structlog.nvim', 
+  },
+    config = function()
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+      require('csharp').setup({
+        server = {
+          capabilities = capabilities,
+          on_attach = function(client, bufnr)
+            local opts = { buffer = bufnr, silent = true }
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+            -- Add more keybindings as needed
+          end,
+        }
+      })
+    end
+  },
+
+  -- Autocompletion Plugins
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'saadparwaiz1/cmp_luasnip',
+      'L3MON4D3/LuaSnip',
+    },
+    config = function()
+      local cmp = require('cmp')
+      local luasnip = require('luasnip')
+
+      cmp.setup({
+        snippet = {
+          expand = function(args)
+            luasnip.lsp_expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-e>'] = cmp.mapping.abort(),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = cmp.config.sources({
+          { name = 'nvim_lsp' },
+          { name = 'luasnip' },
+        }, {
+          { name = 'buffer' },
+          { name = 'path' },
+        })
+      })
+
+      -- Load LuaSnip snippets
+      require('luasnip.loaders.from_vscode').lazy_load()
     end,
   },
-	{
-		'nvimtools/none-ls.nvim',
-		-- event = { "BufReadPre", "BufNewFile" },
-		lazy = true,
-		-- event = "VeryLazy",
-		config = function()
-			local null_ls = require('null-ls')
-			null_ls.setup({
-				sources = {
-					-- null_ls.builtins.formatting.stylua,
-					null_ls.builtins.formatting.csharpier,
-					null_ls.builtins.formatting.yamlfmt,
-					null_ls.builtins.formatting.black,
-					null_ls.builtins.formatting.isort,
-				}
-			})
-			vim.keymap.set('n', '<leader>lff', function() vim.lsp.buf.format({ async = true }) end,
-				{ desc = "Format document" })
-			vim.keymap.set('n', '<leader>lr', vim.lsp.buf.rename, { desc = "Rename Symbol" })
-			vim.keymap.set({ 'n', 'i' }, '<f2>', vim.lsp.buf.rename, { desc = "Rename Symbol" })
-			vim.keymap.set({ 'n', 'i' }, '<f12>', vim.lsp.buf.definition, { desc = "Go to Definition" })
-			vim.keymap.set({ 'n' }, '<leader>ld', vim.lsp.buf.definition, { desc = "Go to Definition" })
-			vim.keymap.set('n', '<leader>li', vim.lsp.buf.implementation, { desc = "Go to Implementation" })
-			vim.keymap.set('n', '<leader>lh', vim.lsp.buf.signature_help, { desc = "Signature Help" })
-			vim.keymap.set('n', '<leader>lsR', vim.lsp.buf.references, { desc = "To to References" })
-			-- vim.keymap.set({ 'n' }, '<leader>lsD', ":Trouble document_diagnostics<CR>", { desc = "Toggle Document Diagnostics" })
-			vim.keymap.set({ 'n' }, '<leader>lsD', ":Trouble diagnostics<CR>", { desc = "Toggle Document Diagnostics" })
-			vim.keymap.set('n', '<leader>lsI', ':Trouble lsp_implementations<CR>',
-				{ desc = "Toggle LSP References" })
-			vim.keymap.set('n', '<leader>lsd', ":Trouble lsp_definitions<CR>", { desc = "Toggle LSP Definitions" })
-		end
-	},
-	{
-		"jay-babu/mason-null-ls.nvim",
-		event = { "BufReadPre", "BufNewFile" },
-		-- event = { 'VeryLazy' },
-		-- enabled = false,
-		dependencies = {
-			"williamboman/mason.nvim",
-			"nvimtools/none-ls.nvim",
-			-- "neovim/nvim-lspconfig"
-		},
-		config = function()
-			require('mason-null-ls').setup({
-				automatic_setup = true
-			})
-		end,
-	}
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,7 @@ return {
       local config = require("nvim-treesitter.configs")
       config.setup({
 	ensure_installed = {"lua", "javascript", "c_sharp"},
+        auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
       })
