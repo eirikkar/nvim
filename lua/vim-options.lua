8fc330b81a84e1f4f69b86ac3cf6417348b768ed
eirikkar <eirikkar@proton.me>
2024-11-28 14:37:13 +0100

    edit keybinds

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -10,8 +10,11 @@ vim.opt.swapfile = false
 -- Navigate vim panes better
 vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
-vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd hCR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.lsp.inlay_hint.enable()
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+vim.keymap.set('n', '<leader>s' , ':w<CR>')
+vim.keymap.set('n', '<leader>r' , ':%s/')
 vim.wo.number = true
+vim.wo.relativenumber = true
