febff2f45890cad9ab43579a8de1f851131526a0
eirikkar <eirikkar@proton.me>
2024-12-03 09:29:17 +0100

    Changed keybinds

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -6,9 +6,9 @@ return {
   },
   config = function()
     require('hop').setup()
-    vim.keymap.set('', 'f', ':HopWord<CR>')
-    vim.keymap.set('', 'F', ':HopChar2<CR>')
-    vim.keymap.set('', 't', ':HopLineStart<CR>')
-    vim.keymap.set('', 'T', ':HopPattern<CR>')
+    vim.keymap.set('', '<leader>f', ':HopWord<CR>')
+    vim.keymap.set('', '<leader>t', ':HopChar2<CR>')
+    vim.keymap.set('', '<leader>F', ':HopLineStart<CR>')
+    vim.keymap.set('', '<leader>w', ':HopPattern<CR>')
  end
 }
