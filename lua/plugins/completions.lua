d9cebe5aa32672f4ba27f14054f39cca9b392dc5
eirikkar <eirikkar@proton.me>
2024-12-13 14:06:40 +0100

    Remove comments

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -46,12 +46,8 @@ return {
       local lspkind = require("lspkind")
       require("nvim-autopairs").setup()
 
-      -- Integrate nvim-autopairs with cmp
       cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-      -- Load snippets
       require("luasnip.loaders.from_vscode").lazy_load()
-
       cmp.setup({
         snippet = {
           expand = function(args)
@@ -90,7 +86,6 @@ return {
           ["<C-c>"] = cmp.mapping.abort(),              -- close completion window
           ["<CR>"] = cmp.mapping.confirm({ select = true }), -- select suggestion
         }),
-        -- sources for autocompletion
         sources = cmp.config.sources({
           { name = "copilot",                group_index = 2 },
           { name = "nvim_lsp",               group_index = 1 }, -- lsp
@@ -99,7 +94,6 @@ return {
           { name = "luasnip",                max_item_count = 3, group_index = 5 }, -- snippets
           { name = "nvim-lsp-signature-help" },
         }),
-        -- Enable pictogram icons for lsp/autocompletion
         formatting = {
           expandable_indicator = true,
           format = lspkind.cmp_format({
