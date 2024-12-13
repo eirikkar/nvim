b373e771072204a8f5ddaceced427a3488ad9028
eirikkar <eirikkar@proton.me>
2024-12-13 13:10:54 +0100

    Changed config in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -9,18 +9,17 @@ return {
     -- import nvim-treesitter plugin
     local treesitter = require("nvim-treesitter.configs")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
+    treesitter.setup({
+      modules = {},
+      ignore_install = {},
+      sync_install = true,
       highlight = {
         enable = true,
       },
-      -- enable indentation
       indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
       autotag = {
         enable = true,
       },
-      -- ensure these language parsers are installed
       ensure_installed = {
         "c_sharp",
         "json",
@@ -44,6 +43,7 @@ return {
         "vimdoc",
         "c",
       },
+      auto_install = true,
       incremental_selection = {
         enable = true,
         keymaps = {
