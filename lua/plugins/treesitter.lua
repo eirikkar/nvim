581b429c806f348d31e91cc4fc2bb3d06c8f476f
eirikkar <eirikkar@proton.me>
2024-12-16 09:31:58 +0100

    Add treewalker

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -55,4 +55,10 @@ return {
       },
     })
   end,
+  {
+    "aaronik/treewalker.nvim",
+    opts = {
+      highlight = true, -- Whether to briefly highlight the node after jumping to it
+    },
+  },
 }
