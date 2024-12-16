2fd9edd738c504a1819ad963dca25b6e3b1d8449
eirikkar <eirikkar@proton.me>
2024-12-16 14:58:49 +0100

    Changed indentation

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,23 +1,23 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").setup {
-          preset = "modern",
-          icons = {
-          mappings = false
-          }
-        }
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").setup({
+					preset = "modern",
+					icons = {
+						mappings = false,
+					},
+				})
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
