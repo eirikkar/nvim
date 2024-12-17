723cc4d25a3e448e805720d7a169b878659d2dc8
eirikkar <eirikkar@proton.me>
2024-12-17 13:40:04 +0100

    Changed keybinds

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -66,31 +66,13 @@ return {
         },
         completion = { completeopt = "menu,menuone,noinsert" },
         mapping = cmp.mapping.preset.insert({
-          ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-          ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
+          ["<C-k>"] = cmp.mapping.select_prev_item {behavior = cmp.SelectBehavior.Insert}, -- previous suggestion
+          ["<C-j>"] = cmp.mapping.select_next_item {behavior = cmp.SelectBehavior.Insert},-- next suggestion
           ["<C-u>"] = cmp.mapping.scroll_docs(-4),      -- scroll up preview
           ["<C-d>"] = cmp.mapping.scroll_docs(4),       -- scroll down preview
           ["<C-Space>"] = cmp.mapping.complete({}),     -- show completion suggestions
           ["<C-c>"] = cmp.mapping.abort(),              -- close completion window
-          ["<CR>"] = cmp.mapping.confirm({ select = true }), -- select suggestion
+          ["<Tab>"] = cmp.mapping.confirm({ select = true }), -- select suggestion
         }),
         sources = cmp.config.sources({
           { name = "copilot",                group_index = 2 },
