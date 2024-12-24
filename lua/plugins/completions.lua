9ab5e8ffc194d889a7b28e1c2170f02f40843466
eirikkar <eirikkar@proton.me>
2024-12-24 13:46:21 +0100

    Add transform items function

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -31,6 +31,15 @@ return {
                         module = "blink-cmp-copilot",
                         score_offset = -100, -- the higher the number, the higher the priority
                         async = true,
+                        transform_items = function(_, items)
+                            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
+                            local kind_idx = #CompletionItemKind + 1
+                            CompletionItemKind[kind_idx] = "Copilot"
+                            for _, item in ipairs(items) do
+                                item.kind = kind_idx
+                            end
+                            return items
+                        end,
                     },
                 },
             },
