c9ae170204b020602c636d0b55ea9704e66e9f39
eirikkar <eirikkar@proton.me>
2025-02-11 10:06:40 +0100

    reformat

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,38 +1,38 @@
 return {
-    {
-        "vague2k/vague.nvim",
-        config = function()
-            require("vague").setup({
-                vim.cmd.colorscheme("vague"),
-                transparent = true,
-                style = {
-                    -- "none" is the same thing as default. But "italic" and "bold" are also valid options
-                    boolean = "bold",
-                    number = "none",
-                    float = "none",
-                    error = "bold",
-                    comments = "italic",
-                    conditionals = "none",
-                    functions = "bold",
-                    headings = "bold",
-                    operators = "none",
-                    strings = "italic",
-                    variables = "none",
+	{
+		"vague2k/vague.nvim",
+		config = function()
+			require("vague").setup({
+				vim.cmd.colorscheme("vague"),
+				transparent = true,
+				style = {
+					-- "none" is the same thing as default. But "italic" and "bold" are also valid options
+					boolean = "bold",
+					number = "none",
+					float = "none",
+					error = "bold",
+					comments = "italic",
+					conditionals = "none",
+					functions = "bold",
+					headings = "bold",
+					operators = "none",
+					strings = "italic",
+					variables = "none",
 
-                    -- keywords
-                    keywords = "none",
-                    keyword_return = "none",
-                    keywords_loop = "none",
-                    keywords_label = "none",
-                    keywords_exception = "none",
+					-- keywords
+					keywords = "none",
+					keyword_return = "none",
+					keywords_loop = "none",
+					keywords_label = "none",
+					keywords_exception = "none",
 
-                    -- builtin
-                    builtin_constants = "none",
-                    builtin_functions = "none",
-                    builtin_types = "none",
-                    builtin_variables = "none",
-                },
-            })
-        end,
-    },
+					-- builtin
+					builtin_constants = "none",
+					builtin_functions = "none",
+					builtin_types = "none",
+					builtin_variables = "none",
+				},
+			})
+		end,
+	},
 }
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,9 +1,10 @@
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=4")
+vim.cmd("set shiftwidth=2")
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.fillchars = { eob = " " }
 vim.g.mapleader = " "
 vim.g.background = "light"
