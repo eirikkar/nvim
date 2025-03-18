5c36436672c5a398afaf0aa0644c26c2a5341e20
eirikkar <eirikkar@proton.me>
2025-03-18 12:08:36 +0100

    removed mason-conform and removed isort, using black for python formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,13 +1,12 @@
 return {
   "stevearc/conform.nvim",
-  "zapling/mason-conform.nvim",
   opts = {},
   config = function()
     require("conform").setup({
       formatters_by_ft = {
         lua = { "stylua" },
         -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
+        python = { "black" },
         -- You can customize some of the format options for the filetype (:help conform.format)
         rust = { "rustfmt", lsp_format = "fallback" },
         -- Conform will run the first available formatter
